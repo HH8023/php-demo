160d29882aac1209c946dbde0a244093738f17dd
smie710 <wp_0357@163.com>
2017-06-28 19:41:37 +0800

    注释 命名 进制

--- mine/628/demo3.php
+++ mine/628/demo3.php
@@ -17,3 +17,13 @@ $bajinzhi = 077;
 //计算：7*8^0 + 7*8^1 = 7 + 56 = 63;
 echo $bajinzhi;
 echo "<br>";
+
+//十进制 直接写
+$shijinzhi = 10001;
+echo $shijinzhi;
+echo "<br>";
+
+//十六进制 表示方法：0x...;
+$hex = 0xff;
+//15*16^0 + 15*16^1 = 15 + 240 =255;
+echo $hex;
